6739954c315694f2c162069e3b847c75914a6207
Michael Orenstein <michael@whiteshadows.me>
2022-06-21 13:21:08 +0930

    chore: Don't halt on the first device to error

--- src/serial/list.ts
+++ src/serial/list.ts
@@ -60,82 +60,87 @@ async function main() {
   );
 
   for (const port of ports) {
-    if (port.path.includes("Bluetooth")) continue;
-    if (port.path.startsWith("/dev/ttyS")) continue; // ignore screens
+    try {
+      if (port.path.includes("Bluetooth")) continue;
+      if (port.path.startsWith("/dev/ttyS")) continue; // ignore screens
 
-    console.log(`connecting to ${port.path}`);
-    const decoder = new StatefulCobsDecoder();
+      console.log(`connecting to ${port.path}`);
+      const decoder = new StatefulCobsDecoder();
 
-    const openedPort = await new Promise<SerialPort>(
-      async (resolve, reject) => {
-        let opened: SerialPort;
+      const openedPort = await new Promise<SerialPort>(
+        async (resolve, reject) => {
+          let opened: SerialPort;
 
-        opened = new SerialPort(port.path, {
-          autoOpen: false,
-          baudRate: 115200,
-        });
+          opened = new SerialPort(port.path, {
+            autoOpen: false,
+            baudRate: 115200,
+          });
 
-        console.log(`port created`);
-        await new Promise((resolve, reject) => setTimeout(resolve, 1000));
-
-        console.log(`opening port`);
-        opened.open(async (err: Error) => {
-          if (err) {
-            reject(err);
-            return;
-          }
-          console.log(`port opened`);
-
+          console.log(`port created`);
           await new Promise((resolve, reject) => setTimeout(resolve, 1000));
 
-          console.log(`rts set`);
-          // Set our port settings immediately
-          opened.set(onAttachmentPortSettingsDefault);
+          console.log(`opening port`);
+          opened.open(async (err: Error) => {
+            if (err) {
+              reject(err);
+              return;
+            }
+            console.log(`port opened`);
 
-          console.log(`connected to ${port.path}`);
-          resolve(opened);
-        });
+            await new Promise((resolve, reject) => setTimeout(resolve, 1000));
 
-        opened.on("data", (data: Buffer) => {
-          console.log(`received data`, data);
-          decoder.receiveCobs(data);
-        });
+            console.log(`rts set`);
+            // Set our port settings immediately
+            opened.set(onAttachmentPortSettingsDefault);
 
-        opened.on("error", (error: Error) => {
-          console.log(`received error from ${port.path}:`, error);
-        });
+            console.log(`connected to ${port.path}`);
+            resolve(opened);
+          });
 
-        opened.on("close", () => {
-          console.log(`closed ${port.path}:`);
-        });
-      }
-    );
+          opened.on("data", (data: Buffer) => {
+            console.log(`received data`, data);
+            decoder.receiveCobs(data);
+          });
 
-    await new Promise((resolve, reject) => setTimeout(resolve, 1000));
+          opened.on("error", (error: Error) => {
+            console.log(`received error from ${port.path}:`, error);
+          });
 
-    console.log(`sending messages:`);
+          opened.on("close", () => {
+            console.log(`closed ${port.path}:`);
+          });
+        }
+      );
 
-    const requestBoardIDMessage = new Message(
-      MESSAGEIDS.BOARD_IDENTIFIER,
-      null
-    );
-    requestBoardIDMessage.metadata.type = TYPES.UINT16;
-    requestBoardIDMessage.metadata.internal = true;
-    requestBoardIDMessage.metadata.query = true;
-
-    for (let index = 0; index < 5; index++) {
-      const requestBuffer = encodeCobs(encode(requestBoardIDMessage));
-      console.log(`writing #${index}`, requestBuffer);
-      openedPort.write(requestBuffer);
-      openedPort.flush();
-
-      // wait a few seconds for a reply
       await new Promise((resolve, reject) => setTimeout(resolve, 1000));
+
+      console.log(`sending messages:`);
+
+      const requestBoardIDMessage = new Message(
+        MESSAGEIDS.BOARD_IDENTIFIER,
+        null
+      );
+      requestBoardIDMessage.metadata.type = TYPES.UINT16;
+      requestBoardIDMessage.metadata.internal = true;
+      requestBoardIDMessage.metadata.query = true;
+
+      for (let index = 0; index < 5; index++) {
+        const requestBuffer = encodeCobs(encode(requestBoardIDMessage));
+        console.log(`writing #${index}`, requestBuffer);
+        openedPort.write(requestBuffer);
+        openedPort.flush();
+
+        // wait a few seconds for a reply
+        await new Promise((resolve, reject) => setTimeout(resolve, 1000));
+      }
+
+      openedPort.close();
+
+      console.log(`finished with ${port.path}`);
+    } catch (e) {
+      console.log(`error with port ${port.path}:`);
+      console.error(e);
     }
-
-    openedPort.close();
-
-    console.log(`finished with ${port.path}`);
   }
 }
 
